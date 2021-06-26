ab68f3c348c69fd52d6b49300ee3f51afa956d32
skogenvandreren <50079524+skogenvandreren@users.noreply.github.com>
2021-06-26 21:28:22 +0300

    commit main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print ('Hello from Амир')
+from json.decoder import JSONDecodeError
+import requests
+
+response = requests.get('https://playground.learnqa.ru/api/hello', params={"name": "test"})
+print(response.text)
+
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text)
+except JSONDecodeError:
+    print('Response is not a JSON format')
