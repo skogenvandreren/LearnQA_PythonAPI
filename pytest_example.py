869d8f663573dfb81670b12b0795da12b02d0043
skogenvandreren <50079524+skogenvandreren@users.noreply.github.com>
2021-08-10 22:03:25 +0300

    Added some files

---
+++ pytest_example.py
@@ -0,0 +1,14 @@
+class Test:
+    def test_check_math(self):
+        a = 5
+        b = 9
+        expected_sum = 14
+        assert a + b == expected_sum, f"Sum of variables a and b is not equal to {expected_sum}"
+
+    def test_check_math2(self):
+        a = 5
+        b = 11
+        expected_sum = 14
+        assert a + b == expected_sum, f"Sum of variables a and b is not equal to {expected_sum}"
+
+# py -m pytest test.py -k "test_check_math" запуск
