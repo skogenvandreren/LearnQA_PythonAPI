45a7ac769efa03422b1203e8f9cebb4ae29a10f9
skogenvandreren <50079524+skogenvandreren@users.noreply.github.com>
2021-08-10 18:16:32 +0300

    Fix Ex7

--- Ex7.py
+++ Ex7.py
@@ -9,25 +9,66 @@ response = requests.head('https://playground.learnqa.ru/ajax/api/compare_query_t
 print('Вывод второй задачи: ' + response.text)
 
 # TASK 3
-method = 'GET'
-response = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type', params=method)
-print('Вывод третьей задачи: ' + response.text)
+method = 'POST'
+response = requests.post('https://playground.learnqa.ru/ajax/api/compare_query_type', data={"method": 'POST'})
+print('Вывод третьей задачи: ' + response.text + ' ' + response.url)
 
 # TASK 4
-methods = ['get', 'post', 'put', 'delete']
+methods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
 requests_methods = [requests.get, requests.post, requests.put, requests.delete]
 request_url = 'https://playground.learnqa.ru/ajax/api/compare_query_type'
 
 for i in methods:
-    requestGet = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type', params=i)
-    print('Вывод четвертой задачи (GET_URL): ' + requestGet.url)
-    print('Вывод четвертой задачи (GET_TEXT): ' + requestGet.text)
-    requestPost = requests.post('https://playground.learnqa.ru/ajax/api/compare_query_type', params=i)
-    print('Вывод четвертой задачи (POST_URL): ' + requestPost.url)
-    print('Вывод четвертой задачи (POST_TEXT): ' + requestPost.text)
-    requestPut = requests.put('https://playground.learnqa.ru/ajax/api/compare_query_type', params=i)
-    print('Вывод четвертой задачи (PUT_URL): ' + requestPut.url)
-    print('Вывод четвертой задачи (PUT_TEXT): ' + requestPut.text)
-    requestDelete = requests.delete('https://playground.learnqa.ru/ajax/api/compare_query_type', params=i)
-    print('Вывод четвертой задачи (DELETE_URL): ' + requestDelete.url)
-    print('Вывод четвертой задачи (DELETE_TEXT): ' + requestDelete.text)
+    if i == 'GET':
+        requestGet = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type', params={"method": 'GET'})
+        print('Вывод четвертой задачи (GET->GET): ' + requestGet.text)
+        requestGet = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type', data={"method": 'POST'})
+        print('Вывод четвертой задачи (GET->POST): ' + requestGet.text)
+        requestGet = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type', data={"method": 'PUT'})
+        print('Вывод четвертой задачи (GET->PUT): ' + requestGet.text)
+        requestGet = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                  data={"method": 'DELETE'})
+        print('Вывод четвертой задачи (GET->DELETE): ' + requestGet.text + '\n')
+
+    if i == 'POST':
+        requestPost = requests.post('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                    data={"method": 'POST'})
+        print('Вывод четвертой задачи (POST->POST): ' + requestPost.text)
+        requestPost = requests.post('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                    params={"method": 'GET'})
+        print('Вывод четвертой задачи (POST->GET): ' + requestPost.text)
+        requestPost = requests.post('https://playground.learnqa.ru/ajax/api/compare_query_type', data={"method": 'PUT'})
+        print('Вывод четвертой задачи (POST->PUT): ' + requestPost.text)
+        requestPost = requests.post('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                    data={"method": 'DELETE'})
+        print('Вывод четвертой задачи (POST->DELETE): ' + requestPost.text + '\n')
+
+    if i == 'PUT':
+        requestPut = requests.put('https://playground.learnqa.ru/ajax/api/compare_query_type', data={"method": 'PUT'})
+        print('Вывод четвертой задачи (PUT->PUT): ' + requestPut.text)
+        requestPut = requests.put('https://playground.learnqa.ru/ajax/api/compare_query_type', params={"method": 'GET'})
+        print('Вывод четвертой задачи (PUT->GET): ' + requestPut.text)
+        requestPut = requests.put('https://playground.learnqa.ru/ajax/api/compare_query_type', data={"method": 'POST'})
+        print('Вывод четвертой задачи (PUT->POST): ' + requestPut.text)
+        requestPut = requests.put('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                  data={"method": 'DELETE'})
+        print('Вывод четвертой задачи (PUT-DELETE): ' + requestPut.text + '\n')
+
+    if i == 'DELETE':
+        requestDelete = requests.delete('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                        data={"method": 'DELETE'})
+        print('Вывод четвертой задачи (DELETE->DELETE): ' + requestDelete.text)
+        requestDelete = requests.delete('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                        params={"method": 'GET'})
+        print('Вывод четвертой задачи (DELETE->GET): ' + requestDelete.text)
+        requestDelete = requests.delete('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                        data={"method": 'POST'})
+        print('Вывод четвертой задачи (DELETE->POST): ' + requestDelete.text)
+        requestDelete = requests.delete('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                        data={"method": 'PUT'})
+        print('Вывод четвертой задачи (DELETE->PUT): ' + requestDelete.text + '\n')
+
+    if i == 'PATCH':
+        requestPatch = requests.patch('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                      data={"method": 'PATCH'})
+        print('Вывод четвертой задачи (PATCH): ' + requestPatch.text)
